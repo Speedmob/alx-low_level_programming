12ab27c589f45edab945574a0bd478f613778f18
Speedmob <oooamalzagh@hotmail.com>
2023-09-14 08:57:28 -0700

    0x04-more_functions_nested_loops-3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two numbers
+ * @a: first multiplication
+ * @b: second multiplication
+ *
+ * Return: Result of multiplication
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
