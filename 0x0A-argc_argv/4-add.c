e86ff5e431095455e548903d05eb6424250d1868
Speedmob <oooamalzagh@hotmail.com>
2023-10-02 12:27:14 -0700

    0x0A-argc_argv-5

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - prints product of two numbers
+ * @argc: num of args passed
+ * @argv: pointer to args passed
+ *
+ * Return: Always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	int x = 0, a, m;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (m = 0; argv[a][m]; m++)
+			if (isdigit(argv[a][m]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
+		x += atoi(argv[a]);
+	}
+	printf("%d\n", x);
+	return (0);
+}
