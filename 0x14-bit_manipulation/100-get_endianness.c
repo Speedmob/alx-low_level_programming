1c3368dea7ed3a1b821fc29cf6c064f5f3a58f9e
Speedmob <oooamalzagh@hotmail.com>
2023-10-26 14:18:57 -0700

    0x14-bit_manipulation-7

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - a function that checks the endianness.
+ *
+ * Return: 0 if big endian, 1 if little endian
+*/
+int get_endianness(void)
+{
+	unsigned int end = 1;
+
+	return ((long int)&end);
+}
