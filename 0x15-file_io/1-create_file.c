8419c1324fd7e09531d001410493f602a5df9498
Speedmob <oooamalzagh@hotmail.com>
2023-10-30 07:39:17 -0700

    0x15-file_io-2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,12 +10,13 @@ int create_file(const char *filename, char *text_content)
 {
 	int f, len;
 	long int w;
+
 	if (filename == NULL)
 		return (-1);
-	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	f = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (f == -1)
 		return (-1);
-	if (filename)
+	if (text_content)
 	{
 		for (len = 0; text_content[len]; len++)
 			;
