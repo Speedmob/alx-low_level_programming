ed24fc31a573424d91d71e752cc9b09296877baf
Speedmob <oooamalzagh@hotmail.com>
2023-10-30 08:00:16 -0700

    0x15-file_io-3

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: file name
+ * @text_content: text content
+ *
+ * Return: 1 (Success), -1 (Failure)
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int f, len;
+	long int w;
+
+	if (filename == NULL)
+		return (-1);
+	f = open(filename, O_RDWR | O_APPEND);
+	if (f == -1)
+		return (-1);
+	if (text_content)
+	{
+		for (len = 0; text_content[len]; len++)
+			;
+		w = write(f, text_content, len);
+		if (w == -1)
+			return (-1);
+	}
+	return (close(f) == -1 ? -1 : 1);
+}
